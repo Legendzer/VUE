64601bbca8dfbe6572b79f7b3b1d0730f5935837
Legendzer <240115210@qq.com>
2021-08-13 10:21:12 +0800

    VUE

--- 13_LearnVuejs/01_webpack的使用/03_webpack的loader/webpack.config.js
+++ 13_LearnVuejs/01_webpack的使用/03_webpack的loader/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'dist/'
     },
     module: {
         rules: [
@@ -15,6 +16,30 @@ module.exports = {
             // 使用多个loader时，是从右往左
             use: [ 'style-loader', 'css-loader']
           },
+          {
+            test: /\.less$/,
+            use: [{
+              loader: "style-loader"
+            },{
+              loader: "css-loader"
+            },{
+              loader: "less-loader"
+            }]
+          },
+          {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  // 当加载的图片小于limit时，会将图片编译成base64字符串的形式,
+                  // 当加载的图片大于limit时，需要使用file-loader模块进行加载。
+                  limit: 15000,
+                  name: 'img/[name].[hash:8].[ext]'
+                },
+              }
+            ]
+          }
         ],
       },
 }
